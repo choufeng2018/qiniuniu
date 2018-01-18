1021e01e02549a1fb7c859cb240a62714802366b
jin <shhuangyan1@163.com>
2018-01-18 16:09:52 +0800

    .

--- application/admin/controller/product.php
+++ application/admin/controller/product.php
@@ -7,6 +7,7 @@
  */
 namespace app\admin\controller;
 use think\Controller;
+use think\Request;
 
 class Product extends Controller{
     public function publishwine(){
@@ -14,6 +15,36 @@ class Product extends Controller{
     }
     public function winelist(){
         return $this->fetch();
+    }
+
+    //添加产品
+    public function addProduct(){
+        $data = Request::instance()->post();
+        $info = Db::table('products')->insert($data);
+        if($info){
+            $date['success'] = 'success';
+            $date['msg'] = '保存成功！';
+        }else{
+            $date['fail'] = 'fail';
+            $date['msg'] = '保存失败！';
+        }
+        echo json_encode($date);
+    }
+
+    //上传封面
+    public function fileUpload(){
+        $file = request()->file('img');
+        // 移动到框架应用根目录/public/upload/ 目录下
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'upload',$savname=false);
+            if($info){
+                $date['src'] = $info->getFilename();
+                echo json_encode($date);
+            }else{
+                // 上传失败获取错误信息
+                echo $file->getError();
+            }
+        }
     }
 
 }
